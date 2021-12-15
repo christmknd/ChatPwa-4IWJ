26fc2b306d7b8ff048334ade794123e2826bcafc
christ <christmokonda@gmail.com>
2021-12-15 11:52:56 +0100

    Salon Component Finish

--- app/src/components/Chat.js
+++ app/src/components/Chat.js
@@ -9,18 +9,12 @@ function Chat (){
     return (
         <>
 
-
-            <span>User : User 1</span>
-
             <form id="input_zone">
             <input id="message" className="vertical-align" type="text"/>
             <button id="send_message" className="vertical-align" type="button">Send</button>
         </form>
             <h2>Listes des messages</h2>
 
-            <ul>
-                <li></li>
-            </ul>
         </>
     )
 
--- app/src/components/Salon.js
+++ app/src/components/Salon.js
@@ -1,25 +1,73 @@
 import React from 'react';
+import {
+    Button,
+    Card,
+    CardContent,
+    CardActions,
+    Grid,
+    makeStyles,
+    Box,
+    Typography,
+    TextField,
+    Link
+} from "@mui/material";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+
 
 function Salon (){
+
+    function createData(username, content, dateTime, isReceived, isView) {
+        return { username, content, dateTime, isReceived, isView };
+    }
+
+    const rows = [
+        createData('Alan', 'zfsfgsz', '15.12.21', 'yes','no'),
+        createData('Laura', 'Jambon Beure', '24.11.21', 'yes','yes'),
+        createData('Jamie', 'ok à toute', '19.10.21', 'yes','yes'),
+    ];
+
     return (
-        <>
 
-            <header>
-                <nav>
-                    <ul>
-                        <li>Home</li>
-                        <li>Salon</li>
-                    </ul>
-                </nav>
-            </header>
+        <Grid
+            container
+            justify="center"
+            align="center"
+            spacing={1}
+            style={{
+                width: "100%",
+                height: "100vh",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Grid item md={10}>
+                <Card>
+                    <CardContent>
+                        <TableContainer>
+                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                <TableBody>
+                                    {rows.map((row) => (
+                                                <TableRow
+                                                    key={row.username}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell align="right"><Link>{row.username}</Link></TableCell>
+                                                    <TableCell align="right">{row.content}</TableCell>
+                                                    <TableCell align="right">{row.dateTime}</TableCell>
+                                                    <TableCell align="right">{row.isReceived}</TableCell>
+                                                    <TableCell align="right">{row.isView}</TableCell>
+                                                </TableRow>
 
-            <ul>
-               <li><a href="#">User 1</a></li>
-               <li><a href="#"> User 2</a></li>
-            </ul>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
 
 
-        </>
     )
 }
 
