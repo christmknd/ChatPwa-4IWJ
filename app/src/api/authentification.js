f552bf3f845c49117c9c39ba8c8c244600b246d1
Maxime Daude <maximedaude.dev@gmail.com>
2021-12-16 20:58:15 +0100

    add auth

--- app/src/api/authentification.js
+++ app/src/api/authentification.js
@@ -1,54 +1,9 @@
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithRedirect} from "firebase/auth";
 import {auth, database} from "./configFirebase";
 import {ref, set} from "firebase/database";
-import firebase from 'firebase/compat/app';
-import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
 
 /// Login
-
-// Initialize the FirebaseUI Widget using Firebase.
-const ui = new firebaseui.auth.AuthUI(auth);
-
-const uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
-        },
-        uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            document.getElementById('loader').style.display = 'none';
-        }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-        auth.EmailAuthProvider+"firebase",
-        {
-            provider: auth.GoogleAuthProvider+"",
-            scopes: [
-                'https://www.googleapis.com/auth/contacts.readonly'
-            ],
-            customParameters: {
-                // Forces account selection even when one account
-                // is available.
-                prompt: 'select_account'
-            }
-        },
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-};
-
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container-auth', uiConfig);
 
 
 // Good
